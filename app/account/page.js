6796a6f0cd8ec7845427d858f7da31de3d295ea2
Victor koome <koomev786@gmail.com>
2025-07-28 09:02:50 +0300

    add nested layouts and routes

--- app/account/page.js
+++ app/account/page.js
@@ -4,6 +4,8 @@ export const metadata = {
 
 export default function Account() {
   return (
-    <h1>Your account page</h1>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Victor
+      </h2>
   );
 }
