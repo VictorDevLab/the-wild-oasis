70ce162f64c082b6b95f8e49b4c0db3b6bd1ad37
Victor koome <koomev786@gmail.com>
2025-09-11 17:01:12 +0300

    Data fetching strategies for the reservation section

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,5 +1,7 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
 import TextExpander from "@/app/_components/TextExpander";
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { getBookedDatesByCabinId, getCabin, getCabins, getSettings } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -19,6 +21,9 @@ export async function generateStaticParams(params) {
 
 export default async function Page({ params }) {
     const cabin = await getCabin(params.cabinId)
+    const settings = await getSettings()
+    const bookedDates = await getBookedDatesByCabinId(params.cabinId)
+
     const { id, name, maxCapacity, regularPrice, discount, image, description } =
         cabin;
 
@@ -64,9 +69,13 @@ export default async function Page({ params }) {
             </div>
 
             <div>
-                <h2 className="text-5xl font-semibold text-center">
-                    Reserve today. Pay on arrival.
+                <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+                    Reserve {name} today. Pay on arrival.
                 </h2>
+                <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+                    <DateSelector />
+                    <ReservationForm />
+                </div>
             </div>
         </div>
     );
